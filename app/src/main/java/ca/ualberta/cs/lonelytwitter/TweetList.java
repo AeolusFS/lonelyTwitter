ec628af7516d146d3e91c2bb74ac39c52fd413f0
aeoluszhai <charlie.zhaibca@gmail.com>
2017-10-13 14:46:54 -0600

    MSG

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
@@ -0,0 +1,39 @@
+package ca.ualberta.cs.lonelytwitter;
+import java.util.ArrayList;
+
+
+/**
+ * Created by zhai on 10/10/17.
+ */
+
+public class TweetList {
+
+    private ArrayList<Tweet> tweets = new ArrayList<Tweet>();
+
+    // add tweet to a List of tweets
+    public void addTweet(Tweet tweet){
+
+        tweets.add(tweet);
+
+    }
+
+    // delete tweet from a list of tweets
+    public void deleteTweet(Tweet tweet){
+
+        tweets.remove(tweet);
+
+    }
+
+    // check if a list of tweets has the specified tweet
+    // if it does, return true; otherwise return false
+    public boolean hasTweet(Tweet tweet) {
+
+        return tweets.contains(tweet);
+    }
+
+    public Tweet getTweet(int index){
+         return tweets.get(index);
+    }
+
+
+}
