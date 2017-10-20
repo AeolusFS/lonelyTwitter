c91c3199ee3e3347e54e924f0dc06f03b45d30be
aeoluszhai <charlie.zhaibca@gmail.com>
2017-10-20 14:59:30 -0600

    lab7

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/EditTweetActivity.java
@@ -0,0 +1,16 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EditTweetActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_tweet);
+
+        Bundle bundle = getIntent().getExtras();
+        String var = bundle.getString("var");
+    }
+}
